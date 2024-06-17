6a02b954e65788d674deb119f1c77d69c80f7f60
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-17 18:58:36 +0200

    testing

--- raspcode/main.py
+++ raspcode/main.py
@@ -3,11 +3,20 @@ import time
 
 diodes = [4,27,22]
 
-for gpio in diodes:
-    print(gpio)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(gpio,GPIO.OUT)
-    GPIO.output(gpio,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(gpio,GPIO.LOW)
+# for gpio in diodes:
+#     print(gpio)
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setup(gpio,GPIO.OUT)
+#     GPIO.output(gpio,GPIO.HIGH)
+#     time.sleep(1)
+#     GPIO.output(gpio,GPIO.LOW)
 
+
+while True:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(4,GPIO.OUT)
+    GPIO.output(4,GPIO.HIGH)
+    GPIO.setup(27,GPIO.OUT)
+    GPIO.output(27,GPIO.HIGH)
+    GPIO.setup(22,GPIO.OUT)
+    GPIO.output(22,GPIO.HIGH)
