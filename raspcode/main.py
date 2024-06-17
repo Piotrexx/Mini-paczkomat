c57e259f83b9d19edc1f8111952a49611f97d796
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-17 18:40:30 +0200

    rasp fix

--- raspcode/main.py
+++ raspcode/main.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
-GPIO.output(18,GPIO.HIGH)
+GPIO.setup(4,GPIO.OUT)
+GPIO.output(4,GPIO.HIGH)
 time.sleep(1)
-GPIO.output(18,GPIO.LOW)
+GPIO.output(4,GPIO.LOW)
