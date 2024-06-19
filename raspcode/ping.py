2a9ddf00c9afd1f7c09ecbc712051a5a23dcd07f
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-19 14:04:01 +0200

    hotfix

--- raspcode/ping.py
+++ raspcode/ping.py
@@ -5,16 +5,16 @@ import socket
 
 
 decision = bool(int(input("Create 0. new or 1. check ? 0/1")))
-
-if decision:
-    url = "/ip/check/"
-else:
-    url = "/paczkomat/add_paczkomat/"
-
 load_dotenv()
 uuid = os.getenv("uuid") # uuid paczkomatu wygenerowane wcześniej
 ip_address = socket.gethostbyname(socket.gethostname()) # adres IP w sieci lokalnej (może kiedyś przerobię to na sięć publiczą)
 BASE_URL = os.getenv("server_url")
 
+if decision:
+    requests.patch(url=f"{BASE_URL}/ip/check/", data={"id": uuid, "ip_address": ip_address})
 
-requests.patch(url=f"{BASE_URL}{url}", data={"id": uuid, "ip_address": ip_address})
+else:
+    requests.patch(url=f"{BASE_URL}/paczkomat/add_paczkomat/", data={"id": uuid, "ip_address": ip_address})
+
+
+
