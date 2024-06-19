9cce7b54288386e829dbbbcd4d792afae819eff2
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-19 13:47:14 +0200

    models, migrations, raspcode

--- backend/backend_app/models.py
+++ backend/backend_app/models.py
@@ -42,7 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name", "phone_no"]
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     
 
--- raspcode/ping.py
+++ raspcode/ping.py
@@ -3,10 +3,18 @@ from dotenv import load_dotenv
 import os
 import socket
 
+
+decision = bool(input("Create 0. new or 1. check ? 0/1"))
+
+if decision:
+    url = "/ip/check/"
+else:
+    url = "/paczkomat/add_paczkomat/"
+
 load_dotenv()
 uuid = os.getenv("uuid") # uuid paczkomatu wygenerowane wcześniej
 ip_address = socket.gethostbyname(socket.gethostname()) # adres IP w sieci lokalnej (może kiedyś przerobię to na sięć publiczą)
 
 
 
-requests.patch(url=os.getenv("server_url"), data={"id": uuid, "ip_address": ip_address})
+requests.patch(url=f"{os.getenv("server_url")}{url}", data={"id": uuid, "ip_address": ip_address})
