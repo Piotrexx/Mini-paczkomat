bf7b401991e6e2995e4eb2f0491ec562ba69d956
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-20 12:45:01 +0200

    przerobiony kod pythona do rusta

---
+++ raspcode/paczkomat_server/src/lib.rs
@@ -0,0 +1,26 @@
+use std::{collections::HashMap, net::IpAddr};
+use local_ip_address::local_ip;
+use dotenv::dotenv;
+
+pub fn return_local_ipaddress() ->  Result<IpAddr,String>{
+    let paczkomat_ip = local_ip();
+    match paczkomat_ip {
+        Ok(ip) => Ok(ip),
+        Err(err) => Err(format!("Wystąpił błąd: {:?}", err))
+    }
+}
+
+
+
+pub fn ping() {
+    dotenv().ok(); 
+    
+    let mut data = HashMap::new();
+    data.insert("id", std::env::var("uuid").expect("Nie znaleziono uuid w pliku .env."));
+    data.insert("ip", return_local_ipaddress().unwrap().to_string());
+
+    let client = reqwest::Client::new();
+    let _res = client.patch({std::env::var("server_url").expect("Nie znaleziono url servera w pliku .env.")} + "/ip/check/")
+    .json(&data)
+    .send();
+}
--- raspcode/paczkomat_server/src/main.rs
+++ raspcode/paczkomat_server/src/main.rs
@@ -1,12 +1,24 @@
 // rust_gpiozero = "0.2.0" tylko na raspberry pi
+use lib::{ping, return_local_ipaddress};
+mod lib;
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/check")]
+fn check() -> String {
+    ping();
+    format!("Wysłano requesta do sprawdzenia IP")
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .configure(rocket::Config::figment().merge(("address", return_local_ipaddress().unwrap())))
+    .mount("/", routes![hello, check])
 }
+
