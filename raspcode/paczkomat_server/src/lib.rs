2f51efc80b2429a14cb4b2e3d4a42b17d156f73b
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-20 13:26:57 +0200

    fix

--- raspcode/paczkomat_server/src/lib.rs
+++ raspcode/paczkomat_server/src/lib.rs
@@ -1,6 +1,8 @@
 use std::{collections::HashMap, net::IpAddr};
 use local_ip_address::local_ip;
 use dotenv::dotenv;
+use reqwest::{Client, Url};
+use serde_json::json;
 
 pub fn return_local_ipaddress() ->  Result<IpAddr,String>{
     let paczkomat_ip = local_ip();
@@ -12,16 +14,33 @@ pub fn return_local_ipaddress() ->  Result<IpAddr,String>{
 
 
 
-pub fn ping() {
+pub async fn ping() {
     dotenv().ok(); 
     
     let mut data = HashMap::new();
     data.insert("id", std::env::var("uuid").expect("Nie znaleziono uuid w pliku .env."));
     data.insert("ip", return_local_ipaddress().unwrap().to_string());
+    let url = format!("{}/ip/check/", &std::env::var("server_url").expect("Nie znaleziono url servera w pliku .env."));
+    let client = Client::new();
+    let uuid = std::env::var("uuid").expect("Nie znaleziono uuid w pliku .env");
+    let ip = return_local_ipaddress().unwrap().to_string();
 
-    let client = reqwest::Client::new();
-    let res = client.patch({std::env::var("server_url").expect("Nie znaleziono url servera w pliku .env.")} + "/ip/check/")
-    .json(&data)
-    .send();
-    println!("Response: {:?}", res)
+    let data = json!({
+        "uuid": uuid,
+        "ip": ip,
+    });
+
+    let response = client
+        .patch(Url::parse(&url).unwrap())
+        .json(&data)
+        .send()
+        .await
+        .unwrap();
+
+    if response.status().is_success() {
+        println!("Request wysłany pomyślnie!");
+    } else {
+        println!("Wystąpił błąd: {}", response.status());
+    }
+    
 }
