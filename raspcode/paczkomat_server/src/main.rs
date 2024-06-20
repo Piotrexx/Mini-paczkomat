76f9814b7b2d126e3283032348e9aa6cc58fb70b
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-20 13:32:43 +0200

    pls work

--- raspcode/paczkomat_server/src/main.rs
+++ raspcode/paczkomat_server/src/main.rs
@@ -1,29 +1,24 @@
 // rust_gpiozero = "0.2.0" tylko na raspberry pi
-// use lib::{ping, return_local_ipaddress};
-// mod lib;
-// #[macro_use] extern crate rocket;
+use lib::{ping, return_local_ipaddress};
+mod lib;
+#[macro_use] extern crate rocket;
 
 
-// #[get("/hello/<name>/<age>")]
-// fn hello(name: &str, age: u8) -> String {
-//     format!("Hello, {} year old named {}!", age, name)
-// }
-
-// #[get("/check")]
-// fn check() -> String {
-//     ping();
-//     format!("Wysłano requesta do sprawdzenia IP")
-// }
-
-// #[launch]
-// fn rocket() -> _ {
-//     rocket::build()
-//     .configure(rocket::Config::figment().merge(("address", return_local_ipaddress().unwrap())))
-//     .mount("/", routes![hello, check])
-// }
-
-use paczkomat_server::ping;
-
-fn main(){
-    ping();
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
 }
+
+#[get("/check")]
+async fn check() -> String {
+    ping().await;
+    format!("Wysłano requesta do sprawdzenia IP")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .configure(rocket::Config::figment().merge(("address", return_local_ipaddress().unwrap())))
+    .mount("/", routes![hello, check])
+}
+
