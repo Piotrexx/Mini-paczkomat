9d5c578a9ddcaba2b185aa12e33733c4d4755f3d
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-07-03 12:26:19 +0200

    LED tests

--- raspcode/paczkomat_server/tests/led_test.rs
+++ raspcode/paczkomat_server/tests/led_test.rs
@@ -4,15 +4,19 @@ mod test {
     #[test]
     fn led_test() {
         use rust_gpiozero::*;
-        use tokio;
+        // use tokio;
         let mut locker = LED::new(4);
-        tokio::spawn(async move {
-            locker.on();
-            loop {
-                // Do nothing here (or add minimal logic)
-                tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
-            }
-        });
+        
+        loop {
+            locker.on()    
+        }
+        // tokio::spawn(async move {
+        //     locker.on();
+        //     loop {
+        //         // Do nothing here (or add minimal logic)
+        //         tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+        //     }
+        // });
     }
 
 }
