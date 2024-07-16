05f5c6f6dd90b9fefe6950ae466b758ab978bc0c
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-07-16 17:42:26 +0200

    debugging

--- raspcode/paczkomat_server/src/functions.rs
+++ raspcode/paczkomat_server/src/functions.rs
@@ -198,8 +198,9 @@ pub async fn create_package(package: Json<Package>) -> Result<String>{
 async fn check(handle: mpsc::Sender<ActorMessage>, locker_id: String) -> bool {
     let (send, recv) = oneshot::channel();
     handle.send(ActorMessage::CheckIfEmpty(send)).await.unwrap();
-    println!("{}", recv.await.unwrap())
-    *recv.await.unwrap().get(&locker_id).unwrap()
+    println!("{:?}", recv.await.unwrap());
+    // recv.await.unwrap().get(&locker_id).unwrap()
+    true
 
 }
 
